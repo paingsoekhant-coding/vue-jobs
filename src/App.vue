85ff981b6805d99d23766459e5f7176c33a891ed
Paing Soe Khant <babyboss7916@gmail.com>
2025-01-04 10:03:42 +0630

    Add Nav bar & Hero Section Use defineProps func in hero section

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeroSection from "./components/Hero-section.vue";
+import Navbar from "./components/Nav-bar.vue";
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <HeroSection
+    title="Hello Vue Developer"
+    subtitle="Find your jobs here."
+  ></HeroSection>
+</template>
