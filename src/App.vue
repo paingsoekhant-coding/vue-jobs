dfb598e2d006ec8511da1c4d27c03e6058a3892a
Paing Soe Khant <babyboss7916@gmail.com>
2025-01-04 10:43:48 +0630

    add card & job list json file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import HeroSection from "./components/Hero-section.vue";
-import Navbar from "./components/Nav-bar.vue";
+import HeroSection from "@/components/Hero-section.vue";
+import Navbar from "@/components/Nav-bar.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobList from "@/components/JobList.vue";
 </script>
 
 <template>
   <Navbar></Navbar>
-  <HeroSection
-    title="Hello Vue Developer"
-    subtitle="Find your jobs here."
-  ></HeroSection>
+  <HeroSection> </HeroSection>
+  <HomeCards></HomeCards>
+  <JobList></JobList>
 </template>
---
+++ src/components/JobList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import jobData from "@/jobs.json";
+import { ref } from "vue";
+
+const jobs = ref(jobData);
+console.log(jobs.value);
+</script>
+
+<template>
+  <h2>Job List</h2>
+</template>
