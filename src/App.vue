b19294db0a4ed60b95f5147dd7badac64907a208
Paing Soe Khant <babyboss7916@gmail.com>
2025-01-05 14:27:36 +0630

    loop job list json

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import HeroSection from "@/components/Hero-section.vue";
 import Navbar from "@/components/Nav-bar.vue";
 import HomeCards from "@/components/HomeCards.vue";
-import JobList from "@/components/JobList.vue";
+import JobLists from "@/components/JobLists.vue";
 </script>
 
 <template>
   <Navbar></Navbar>
   <HeroSection> </HeroSection>
   <HomeCards></HomeCards>
-  <JobList></JobList>
+  <JobLists></JobLists>
 </template>
---
+++ src/components/JobLists.vue
@@ -0,0 +1,28 @@
+<script setup>
+import data from "@/jobs.json";
+import { ref } from "vue";
+import JobList from "@/components/JobList.vue";
+
+const jobLists = ref(data["jobs"]);
+// console.log(jobLists.value);
+</script>
+
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobList v-for="job in jobLists" :key="job.id" :job="job"></JobList>
+      </div>
+      <section class="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+          >View All Jobs</a
+        >
+      </section>
+    </div>
+  </section>
+</template>
